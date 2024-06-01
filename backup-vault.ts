c9d05f8e56152c3684a3b31bb70b2cc425beec40
Abubakar Balogun <abzynobalogun@gmail.com>
2024-06-01 17:57:42 +0200

    Implement some compress and encrypt functions

--- backup-vault.ts
+++ backup-vault.ts
@@ -2,31 +2,79 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import crypto from "node:crypto";
 import zlib from "node:zlib";
-import { R2Bucket } from "@cloudflare/workers-types";
+import { R2Bucket, R2Object } from "@cloudflare/workers-types";
+import { buffer } from "stream/consumers";
 
-const accountId = "9e56442d6d939701bd12cbdd4390ca3f";
-const bucketName = "obsidian-vaults-backup";
+const accountId = process.env.ACCOUNT_ID;
+const bucketName = process.env.BUCKET_NAME;
 
 const encryptionKey = "";
 
 const r2 = new R2Bucket({ accountId, bucketName });
 
-async function calculateHash(filePath: string): Promise<string> {}
+async function calculateFileHash(filePath: string): Promise<string> {
+    let r2Bucket: R2Object | null = null;
 
-async function compressFile(filePath: string): Promise<string> {}
 
-async function encryptFile(filePath: string): Promise<Buffer> {}
 
-async function uploadToR2(filePath: string): Promise<void> {}
+  const fileBuffer = await fs.readFile(filePath);
+  const hash = crypto.createHash("sha256");
+  hash.update(fileBuffer);
+
+  return hash.digest("hex");
+}
+
+async function compressFile(filePath: string): Promise<Buffer> {
+  const fileBuffer = await fs.readFile(filePath);
+
+  return new Promise<Buffer>((resolve, reject) => {
+    zlib.gzip(fileBuffer, (error, compressedBuffer) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(compressedBuffer);
+      }
+    });
+  });
+}
+
+async function encryptFile(filePath: string): Promise<Buffer> {
+  const fileBuffer = await fs.readFile(filePath);
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv("aes-256-cbc", encryptionKey, iv);
+  const encryptBuffer = Buffer.concat([
+    iv,
+    cipher.update(fileBuffer),
+    cipher.final(),
+  ]);
+
+  return encryptBuffer;
+}
+
+async function uploadToR2(filePath: string): Promise<void> {
+    const key = path.relative(backupObsidianVault, filePath);
+    const fileHash = await calculateFileHash(filePath)
+    let r2Object: R2Object | null = null;
+
+
+    try {
+        r2Object = r2.get(key)
+    } catch (error) {
+        if(error instanceof Error && error.message.includes("Not Found")){
+            const compressedFile = await compressFile(filePath);
+            const encryptedFile = await encryptFile(compressedFile)
+        }
+    }
+}
 
 function calculateDiff(oldBuffer: Buffer, newBuffer: Buffer): Buffer {
   return newBuffer;
 }
-
-async function validateBackups() {}
 async function getBackupKeys(): Promise<string[]> {}
 
-async function backupObsidianVault() {}
+export async function validateBackups() {}
+
+export async function backupObsidianVault() {}
 
 async function getLocalFiles(filePath: string): Promise<string[]> {}
 
